e2670bc7768271bd7e12f46e22f6d0a853301061
seneca <senecamartin11@gmail.com>
2021-09-24 11:49:38 +0200

    Initial Commit

---
+++ Shared/SenecaHeroesMultiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SenecaHeroesMultiApp.swift
+//  Shared
+//
+//  Created by seneca on 24/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SenecaHeroesMultiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
